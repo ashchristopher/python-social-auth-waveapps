9e3e0d1484187e0afb51e94f4ab2a8f97e414f93
Ash Christopher <ash.christopher@gmail.com>
2014-01-13 23:06:34 -0500

    Bumped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-social-auth-waveapps',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     author='Ash Christopher',
     author_email='ash.christopher@gmail.com',
